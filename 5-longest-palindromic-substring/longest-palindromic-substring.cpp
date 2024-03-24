0bb159b751d5062b94aedc2da4ab6dff9ecc24cc
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-03-24 18:26:27 +0530

    Time: 239 ms (27.47%) | Memory: 304 MB (9.21%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    std::string longestPalindrome(std::string s) {
+        std::string ans;
+        int maxlength = 0;
+        int n = s.size();
+        std::vector<std::vector<int>> dp(n, std::vector<int>(n, 0));
+        for (int diff = 0; diff < n; diff++) {
+            for (int i = 0, j = i + diff; j < n; i++, j++) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else if (diff == 1) {
+                    dp[i][j] = (s[i] == s[j]) ? 2 : 0;
+                } else {
+                    if (s[i] == s[j] && dp[i+1][j-1] != 0) {
+                        dp[i][j] = dp[i+1][j-1] + 2;
+                    }
+                }
+                if (dp[i][j]) {
+                    if (j - i + 1 > maxlength) {
+                        maxlength = j - i + 1;
+                        ans = s.substr(i, maxlength);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
