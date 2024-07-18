f2486521a0f655c1b5cdd52b141eac82a859f2ac
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-07-18 17:14:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (6.68%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,30 +1,21 @@
-class Solution{
-    public:
-    void sortColors(std::vector<int>& nums) {
-    int start = 0; 
-    int mid = 0;
-    int end = nums.size()-1;
-    while(mid <= end){
-        switch(nums[mid]){
-            case 0:
-                swap(nums, start, mid);
-                start++;
-                mid++;
-                break;
-            case 1:
-                mid++;
-                break;
-            case 2:
-                swap(nums, mid, end);
-                end--;
-                break;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low =0 ; int mid = 0 ; int high =nums.size()-1;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap (nums[mid], nums[low]);
+                mid++ ;
+                low++ ;
+            }
+            else if(nums[mid] == 1){
+                    mid++ ;
+                }
+                else{
+                    swap(nums[mid],nums[high]);
+                    high-- ;
+                }
+            
         }
     }
-}
-private:
- void swap(std::vector<int>& arr, int pos1, int pos2){
-    int temp = arr[pos1];
-    arr[pos1] = arr[pos2];
-    arr[pos2] = temp;
-}
 };
