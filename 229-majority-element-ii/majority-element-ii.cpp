13fd8c50b35a59a48513d4d6c2b05e68f229bc11
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-07-22 10:55:11 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (31.45%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,52 +1,21 @@
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        // Initialize variables to track potential candidates and their counts
-        int candidate1 = 0, candidate2 = 0;
-        int count1 = 0, count2 = 0;
-
-        // Step 1: Find potential candidates for majority elements
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        int n = nums.size();
+        vector<int>ls;
+        for(int i= 0 ;i<n ;i++){
+              if(ls.size() == 0 || ls[0] != nums[i]){
+                      int cnt = 0;
+            for(int j=0 ;j<n ; j++){
+         if(nums[i] == nums[j]){
+            cnt++ ;
+         }
             }
+            if(cnt > (n/3))
+            ls.push_back(nums[i]);
         }
-
-        // Step 2: Count the occurrences of potential candidates
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            }
-        }
-
-        // Step 3: Check if potential candidates occur more than n/3 times
-        vector<int> result;
-        if (count1 > nums.size() / 3) {
-            result.push_back(candidate1);
-        }
-        if (count2 > nums.size() / 3) {
-            result.push_back(candidate2);
-        }
-
-        return result;
+        if(ls.size() == 2) break;
+    }
+    return ls;
     }
 };
