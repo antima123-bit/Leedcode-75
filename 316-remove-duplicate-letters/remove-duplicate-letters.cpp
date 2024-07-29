ba7ec846b8da37cdf8810230715c76d1622be946
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-07-29 14:46:19 +0530

    Time: 2 ms (63.16%) | Memory: 8.4 MB (74.87%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        string ans;
-        vector<int>count(128);
-        vector<int>used(128);
-        for(const char c: s)
-        ++count[c];
-        for(const char c:s){
-            --count[c];
-            if(used[c])
-            continue;
-          while(!ans.empty() && ans.back() > c && count[ans.back()]){
-          used[ans.back()] = false;
-          ans.pop_back();
-            }
-            used[c] =true;
-            ans.push_back(c);
+       string ans;
+       vector<int>count(128);
+       vector<bool>used(128);
+       for(const char c:s)
+       ++count[c];
+       for(const char c:s){
+        --count[c];
+        if(used[c])
+        continue;
+        while(!ans.empty() && ans.back() > c && count[ans.back()]){
+            used[ans.back()] = false;
+            ans.pop_back();
         }
-        return ans;
+        used[c] = true;
+        ans.push_back(c);
+       } 
+       return ans;
     }
 };
