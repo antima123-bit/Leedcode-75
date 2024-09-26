35d25df499ae403a2bced2b9f5f91c1aa38d9405
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-09-26 21:38:47 +0530

    Time: 434 ms (43.92%) | Memory: 12.3 MB (93.46%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.py
@@ -0,0 +1,26 @@
+class MyCalendar(object):
+
+    def __init__(self):
+       self.calendar = [] 
+
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
+
+        for event in self.calendar:  
+            existing_start, existing_end = event
+
+            if start < existing_end and  end > existing_start:
+               return False
+
+        self.calendar.append((start,end))
+        return True
