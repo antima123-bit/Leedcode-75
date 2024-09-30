eb87d3ed130983966c1407aa1c8d58ec4c24e5d9
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-09-30 20:22:25 +0530

    Time: 55 ms (98.89%) | Memory: 12.4 MB (73.33%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.py
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.py
@@ -1,27 +1,27 @@
 class CustomStack:
 
     def __init__(self, maxSize):
-        self.stack = []  # Stack to store elements
-        self.increment_arr = [0] * maxSize  # Auxiliary array for lazy increments
-        self.maxSize = maxSize  # Maximum size of the stack
+        self.stack = []  
+        self.increment_arr = [0] * maxSize  
+        self.maxSize = maxSize  
 
     def push(self, x):
         if len(self.stack) < self.maxSize:
-            self.stack.append(x)  # Add the element to the stack
+            self.stack.append(x)  
 
     def pop(self):
         if not self.stack:
-            return -1  # Return -1 if stack is empty
+            return -1  
 
-        index = len(self.stack) - 1  # Get the index of the top element
+        index = len(self.stack) - 1  
         if index > 0:
-            self.increment_arr[index - 1] += self.increment_arr[index]  # Transfer the increment to the next element
+            self.increment_arr[index - 1] += self.increment_arr[index]  
 
-        popped_value = self.stack.pop() + self.increment_arr[index]  # Apply the increment to the popped value
-        self.increment_arr[index] = 0  # Reset the increment for this element
-        return popped_value  # Return the popped value with increment
+        popped_value = self.stack.pop() + self.increment_arr[index]  
+        self.increment_arr[index] = 0  
+        return popped_value  
 
     def increment(self, k, val):
-        limit = min(k, len(self.stack)) - 1  # Find the number of elements to increment
+        limit = min(k, len(self.stack)) - 1  
         if limit >= 0:
-            self.increment_arr[limit] += val  # Lazily increment up to the limit
+            self.increment_arr[limit] += val  
