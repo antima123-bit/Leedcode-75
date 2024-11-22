e2fc687890ecce3f7f53a4d6c3414274aa33c655
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2024-11-22 18:08:15 +0530

    Time: 219 ms (10.15%) | Memory: 83.5 MB (18.66%) - LeetSync

--- 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-        unordered_map<string,int> patternCount;
+        unordered_map<string , int> patternCount;
         int maxCount = 0;
         for(auto &row : matrix){
-            string original =" " , flipped = " " ;
-            for (int val : row){
+            string original =" " , flipped =" ";
+            for(int val : row){
                 original += to_string(val);
                 flipped += to_string(1-val);
+
             }
-            patternCount[original]++;
+            patternCount[original]++ ;
             patternCount[flipped]++ ;
-            maxCount  = max(maxCount , max(patternCount[original] ,patternCount[flipped]));
+            maxCount = max(maxCount, max(patternCount[original] , patternCount[flipped]));
         }
         return maxCount;
     }
 };
+
+
+
+
+
+
+
+
