587f9c847d56b94f98486f831f5131a76fbaf881
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-01-03 13:44:17 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (41.08%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,8 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1 , len(nums)):
-                if nums[i] + nums[j] == target:
-                    return (i,j)
-        return -1
+        num_dict={};
+        for i , num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement] , i];
+            num_dict[num] = i
+
+        return None
