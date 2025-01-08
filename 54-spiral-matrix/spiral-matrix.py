6eaf6020c156d12cbf11dac02b4c13584365d21b
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-01-08 13:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (31.27%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,41 +1,38 @@
 class Solution:
-  def spiralOrder(self, matrix):
-    if not matrix:
-      return []
+    def spiralOrder(self, matrix):
+        if not matrix:
+            return []
 
-    m = len(matrix)
-    n = len(matrix[0])
-    ans = []
-    r1 = 0
-    c1 = 0
-    r2 = m - 1
-    c2 = n - 1
+        # Matrix dimensions
+        m = len(matrix)
+        n = len(matrix[0])
+        ans = []
 
-    while len(ans) < m * n:
-      j = c1
-      """Tranverse the top row"""
-      while j <= c2 and len(ans) < m * n:
-        ans.append(matrix[r1][j])
-        j += 1
-        """move to next row"""
-      i = r1 + 1
-      """transverse the right column"""
-      while i <= r2 - 1 and len(ans) < m * n:
-        ans.append(matrix[i][c2])
-        i += 1
-      j = c2
-      """transverse thebottom column"""
-      while j >= c1 and len(ans) < m * n:
-        ans.append(matrix[r2][j])
-        j -= 1
-      i = r2 - 1
-      """transverse the left column"""
-      while i >= r1 + 1 and len(ans) < m * n:
-        ans.append(matrix[i][c1])
-        i -= 1
-      r1 += 1
-      c1 += 1
-      r2 -= 1
-      c2 -= 1
+        # Boundaries
+        r1, c1 = 0, 0
+        r2, c2 = m - 1, n - 1
 
-    return ans
+        # Traverse the matrix in a spiral order
+        while len(ans) < m * n:
+            # Traverse the top row
+            for j in range(c1, c2 + 1):
+                ans.append(matrix[r1][j])
+            # Traverse the right column
+            for i in range(r1 + 1, r2 + 1):
+                ans.append(matrix[i][c2])
+            # Traverse the bottom row (if not already visited)
+            if r1 < r2:
+                for j in range(c2 - 1, c1 - 1, -1):
+                    ans.append(matrix[r2][j])
+            # Traverse the left column (if not already visited)
+            if c1 < c2:
+                for i in range(r2 - 1, r1, -1):
+                    ans.append(matrix[i][c1])
+
+            # Move the boundaries inward
+            r1 += 1
+            c1 += 1
+            r2 -= 1
+            c2 -= 1
+
+        return ans
