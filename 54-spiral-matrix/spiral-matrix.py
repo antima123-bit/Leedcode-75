bc46f3a1b860b297e520e2796903b8e7e2348d14
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-01-08 12:30:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (5.60%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -11,21 +11,25 @@ class Solution:
     r2 = m - 1
     c2 = n - 1
 
-    # Repeatedly add matrix[r1..r2][c1..c2] to `ans`.
     while len(ans) < m * n:
       j = c1
+      """Tranverse the top row"""
       while j <= c2 and len(ans) < m * n:
         ans.append(matrix[r1][j])
         j += 1
+        """move to next row"""
       i = r1 + 1
+      """transverse the right column"""
       while i <= r2 - 1 and len(ans) < m * n:
         ans.append(matrix[i][c2])
         i += 1
       j = c2
+      """transverse thebottom column"""
       while j >= c1 and len(ans) < m * n:
         ans.append(matrix[r2][j])
         j -= 1
       i = r2 - 1
+      """transverse the left column"""
       while i >= r1 + 1 and len(ans) < m * n:
         ans.append(matrix[i][c1])
         i -= 1
