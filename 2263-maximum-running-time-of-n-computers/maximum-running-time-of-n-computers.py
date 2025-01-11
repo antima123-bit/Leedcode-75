f9be0c66167f41fb1b15093002c9b4d9cf22958c
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-01-11 11:44:39 +0530

    Time: 1557 ms (83.33%) | Memory: 21.1 MB (16.67%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
@@ -5,7 +5,6 @@ class Solution(object):
         :type batteries: List[int]
         :rtype: int
         """
-        # Here we categorize the value into Max, Min, Mid to rectify the maximum element
         total_energy = sum(batteries)
         low, high = 0, total_energy // n
 
@@ -14,7 +13,6 @@ class Solution(object):
 
             required_energy = n * mid
 
-            # Now check if the required energy is valid to run the computers
             available_energy = sum(min(b, mid) for b in batteries)
             if available_energy >= required_energy:
                 low = mid
