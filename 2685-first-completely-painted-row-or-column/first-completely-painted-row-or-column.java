19e3d8fae67ddb21c3b0b1b0a36972896e5d850b
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-01-20 17:31:38 +0530

    Time: 27 ms (49.25%) | Memory: 66.2 MB (55.72%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -6,28 +6,27 @@ class Solution {
         //step4.smallest integer i means firstly we see that at which index value the ror/col marked
         //step5.if arr={1,3,2,4,5} in this case if 1,2,3 marked the ans.should be arr[2] 
         //output is 2
-       int m =mat.length;
+       int m = mat.length;
        int n = mat[0].length;
-       int[] rowCount = new int[m];
-       int[] colCount = new int[n];
+       int rowCount[] = new int[m];
+       int colCount[] = new int [n]; 
        //here we create the map where we see the marked and unmarked elements
-       Map<Integer,int[]> positionMap = new HashMap<>();
-       for(int i = 0;i<m ; i++){
-        for(int j = 0 ; j<n ; j++){
-            positionMap.put(mat[i][j] , new int[]{i,j});
+      Map<Integer, int[]> positionMap= new HashMap<>();
+      for(int i = 0;i<m ; i++){
+        for (int j = 0 ; j<n ; j++){
+         positionMap.put(mat[i][j] , new int[]{i,j});
+
         }
-       }
-       for(int i = 0 ;i< arr.length ; i++){
+      }
+     for(int i = 0 ; i< arr.length ; i++){
         int val = arr[i];
         int[] pos = positionMap.get(val);
-        int row =pos[0];
+        int row = pos[0];
         int col = pos[1];
-
         rowCount[row]++ ;
-        colCount[col]++ ; 
-
-        if(rowCount[row] == n || colCount[col] ==m){
-            return i;
+        colCount[col]++ ;
+        if(rowCount[row] == n|| colCount[col] == m){
+            return i ;
         }
        }
        return -1;
