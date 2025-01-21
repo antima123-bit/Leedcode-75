671c23aa1be3637f6677aa2920ce27273b2b8805
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-01-21 10:37:16 +0530

    Time: 15 ms (39.79%) | Memory: 44.5 MB (78.44%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -3,7 +3,7 @@ public class Solution {
         int[] count = new int[3];
         int left = 0, result = 0, n = s.length();
 
-        for (int right = 0; right < n; right++) {
+        for (int right = 0 ; right<n ; right++) {
             count[s.charAt(right) - 'a']++;
 
             while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
