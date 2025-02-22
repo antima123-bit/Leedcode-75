bc8dc94d012166bac76ecb2c2323621635ad7427
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-02-22 23:46:35 +0530

    Time: 18 ms (82.37%) | Memory: 56.1 MB (74.92%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,17 @@
+// TC: O N , SC: O N
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && (i - map.get(nums[i]) <= k)) {
+                return true; // Found the pair.
+            }
+            map.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
+
+    
