46445b13ff6a454198fdcec018feaf14ae61d05a
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-02-26 20:14:06 +0530

    Time: 3 ms (86.18%) | Memory: 46.2 MB (24.69%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,23 +1,15 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] freq= new int[26];
+        int [] freq = new int[26];
         for(char task: tasks){
-            freq[task-'A']++ ;
+            freq[task - 'A']++ ;
         }
-        int maxFreq = 0 ;
-        int maxFreqCount = 0;
-        for(int f: freq){
-            if(f>maxFreq){
-                maxFreq = f;
-                maxFreqCount = 1;
-            }else if(f == maxFreq){
-                maxFreqCount++ ;
-            }
+        Arrays.sort(freq);
+        int chunk = freq[25] - 1;
+        int idle =  chunk * n ;
+        for(int i = 24 ; i>= 0 ; i--){
+            idle -= Math.min(chunk , freq[i]);
         }
-        int partCount = maxFreq - 1;//here we calculate the partiyion space 
-        int emptySlots = partCount*(n+1 - maxFreqCount);
-        int avaiableTasks = tasks.length - (maxFreq * maxFreqCount);
-        int idleSlots = Math.max(0, emptySlots- avaiableTasks);
-    return tasks.length +idleSlots;
+        return idle<0 ? tasks.length:tasks.length + idle;
     }
 }
