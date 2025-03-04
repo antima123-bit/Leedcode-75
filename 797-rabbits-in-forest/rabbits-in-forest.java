44659dfa5210186f39525f0e3696cc4280726e43
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-03-04 20:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (58.93%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,20 +1,16 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        HashMap<Integer , Integer> countMap = new HashMap<>();
-        for(int answer: answers){
-            countMap.put(answer, countMap.getOrDefault(answer,0)+1);
+        int n = answers.length;
+        int[] count = new int[1000];
+        int rabbits = 0 ; 
+        for(int i = 0 ; i< n ; i++){
+        if(count[answers[i]] == 0){
+            rabbits+= answers[i] +1;
+            count[answers[i]]= answers[i];
+        }else{
+            count[answers[i]]-- ;
         }
-        int totalRabbit = 0 ;
-        for(int x: countMap.keySet()){
-            int count = countMap.get(x);
-            int groupSize = x + 1;
-            int group = 0;
-            while(count > 0){
-              count -= groupSize;
-              group++;
-            }
-            totalRabbit += groupSize * group;
         }
-        return totalRabbit;
+        return rabbits;
     }
 }
