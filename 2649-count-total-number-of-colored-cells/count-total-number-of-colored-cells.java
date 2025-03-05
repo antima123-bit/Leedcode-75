e77fc2b5f7d825cd2acae606aec94d7259d52eb3
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-03-05 13:51:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (9.84%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,5 +1,7 @@
 class Solution {
     public long coloredCells(int n) {
+
+        //here we used the formula that generated the coloured grid n^2 +(n-1)^2 this is the formula
     return (long)Math.pow(n, 2) + (long)Math.pow((n-1),2);
     }
 }
