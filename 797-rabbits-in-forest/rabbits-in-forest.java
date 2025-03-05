827d36f48276b7a814537e9b20b3c042a4982e77
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-03-05 13:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (96.38%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,16 +1,17 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        int n = answers.length;
-        int[] count = new int[1000];
-        int rabbits = 0 ; 
-        for(int i = 0 ; i< n ; i++){
-        if(count[answers[i]] == 0){
-            rabbits+= answers[i] +1;
-            count[answers[i]]= answers[i];
-        }else{
+    int n = answers.length;
+    int[]count =new int[1000];
+    int rabbits = 0 ;
+    for(int i = 0 ; i< n ;i++){
+        if(count[answers[i]]== 0){
+        rabbits+=answers[i] +1 ;
+        count[answers[i]] =answers[i];
+        }
+        else{
             count[answers[i]]-- ;
         }
-        }
-        return rabbits;
+    }    
+    return rabbits;
     }
 }
