8108577de3dee1c521cae0542c106c72a2aee6fc
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-03-06 22:48:22 +0530

    Time: 5 ms (38.48%) | Memory: 44.7 MB (22.70%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,29 +1,28 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        HashMap<Character, Integer> FreqMap = new HashMap<>();
-        for(char c : words[0].toCharArray()){
-            FreqMap.put(c,FreqMap.getOrDefault(c, 0)+1);
-        }
-        for(int i = 1 ; i< words.length ; i++){
-            HashMap<Character, Integer> wordMap = new HashMap<>();
-            for(char c: words[i].toCharArray()){
-                wordMap.put(c, wordMap.getOrDefault(c, 0)+1);
-            }
-            for(char c: FreqMap.keySet().toArray(new Character[0])){
-                if(wordMap.containsKey(c)){
-                    FreqMap.put(c, Math.min(FreqMap.get(c) , wordMap.get(c)));
-                }else{
-                   FreqMap.remove(c);
+        List<String> result = new ArrayList<>();
+        
+        for (char c = 'a'; c <= 'z'; c++) {
+            int minCount = Integer.MAX_VALUE;
+            
+            for (String word : words) {
+                int count = 0;
+                for (char ch : word.toCharArray()) {
+                    if (ch == c) {
+                        count++;
+                    }
+                }
+                minCount = Math.min(minCount, count);
+                if (minCount == 0) {
+                    break;
                 }
             }
-        }
-        List<String> result = new ArrayList<>();
-        for(char c: FreqMap.keySet()){
-            int count = FreqMap.get(c);
-            for(int j =  0; j< count ; j++){
+            
+            for (int i = 0; i < minCount; i++) {
                 result.add(String.valueOf(c));
             }
         }
+        
         return result;
     }
 }
