99841a565c364fa530aab2f583d481bdb16e0ed0
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-03-07 13:52:30 +0530

    Time: 1 ms (81.37%) | Memory: 41.4 MB (87.51%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,31 +1,20 @@
 class Solution {
     public String customSortString(String order, String s) {
-        HashMap<Character , Integer> freqCount = new HashMap<>();
-        for(char c: s.toCharArray()){
-            freqCount.put(c, freqCount.getOrDefault(c, 0)+1);
-        }
-        List<Character> result = new ArrayList<>();
-        for(char c: order.toCharArray()){
-            //if the character is present in string then we get the frequncy of the caharcter and if coutn is less then 0 then add to the result otherewise remove from the result 
-            if(freqCount.containsKey(c)){
-                int count = freqCount.get(c);
-                while(count-->0){
-                    result.add(c);
+         StringBuilder result = new StringBuilder();
+        char[] orderCharArray = order.toCharArray();
+        char[] inputCharArray = s.toCharArray();
+        for (char orderChar: orderCharArray){
+            for (char inputChar: inputCharArray){
+                if (inputChar == orderChar){
+                    result.append(inputChar);
                 }
-                freqCount.remove(c);
             }
         }
-        for(char c: freqCount.keySet()){
-            int count = freqCount.get(c);
-            while(count-->0){
-                result.add(c);
+        for (char c: inputCharArray){
+            if (order.indexOf(c) == -1){
+                result.append(c);
             }
         }
-        StringBuilder finalresult = new StringBuilder();
-        for(char c : result){
-            finalresult.append(c);
-        }
-        return finalresult.toString();
+        return result.toString();  
     }
-
 }
